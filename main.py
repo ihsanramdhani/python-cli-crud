f7655ad5fa19fbd839ebd1d5569fcc93e72a6b28
ihsanramdhani <ihsann.ramdhani@gmail.com>
2022-11-05 00:44:24 +0700

    refactor main.py

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
 from core.menu import Menu
+from helper.helper import Helper
 
 class Main:
     def __init__(self):
         self.menu = Menu()
+        self.helper = Helper()
 
     def run(self):
-
-        print(" ""\x1B[4m" +
-        "                                        "
-        + "\x1B[0m")
-        text = "Welcome to the CRUD Program"
-        centered_text = text.center(40)
-        print("|" + centered_text + "|")
-        print("|Please choose any avalaible menu below: |")
-        print("|1. Create Data                          |")
-        print("|2. Get all data                         |")
-        print("|3. Get specific data                    |")
-        print("|4. Update Data                          |")
-        print("|"+
-        "\x1B[4m" + "5. Delete data                          "
-        + "\x1B[0m" + "|")
-
+        self.helper.header()
         choice = input("Choose menu: ")
         self.menu.get_menu(choice)
 
